d0f932744149888e9f89b67b540891789c2cab2e
Jrmy1999 <75275597+Jrmy1999@users.noreply.github.com>
2023-04-02 01:43:41 +0200

    Update suiviEtudiants.php

--- pages/suiviEtudiants.php
+++ pages/suiviEtudiants.php
@@ -1,12 +1,23 @@
 <?php
 session_start();
+
+// inclusion du fichier de configuration de la base de données
 include "../application_config/db_class.php";
+
+// inclusion des fichiers header et navbar
 include 'header.php';
 include 'navbar.php';
-$conn = Database::connect();
-$annee = date('Y');
-?>
 
+// création de la connexion à la base de données
+$conn = Database::connect();
+
+// récupération de l'année en cours
+$annee = date('Y');
+
+// récupération de l'id du tuteur
+$userId = $_SESSION['user'];
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -14,19 +25,20 @@ $annee = date('Y');
 <head>
 	<title>Suivi étudiant</title>
 	<link rel="stylesheet" href="../css/MDB/css/datatables.min.css">
+</head>
 
 <body>
 
 	<?php
-	$query =
-		'SELECT utilisateur.ID_Utilisateur, utilisateur.nom_Utilisateur, utilisateur.Mail_Utilisateur, utilisateur.Promo_Utilisateur, utilisateur.HuitClos_Utilisateur,
-invite.Entreprise_Invite, invite.Ville_Invite, invite.Nom_Invite, invite.Mail_Invite FROM utilisateur 
- LEFT JOIN est_apprenti ON utilisateur.ID_Utilisateur = est_apprenti.ID_Utilisateur 
- LEFT JOIN invite ON est_apprenti.ID_Invite = invite.ID_Invite 
- JOIN habilitations ON utilisateur.ID_Utilisateur = habilitations.ID_Utilisateur 
-WHERE habilitations.Etudiant_Habilitations LIKE "oui" AND utilisateur.Annee_utilisateur LIKE "%' . $annee . '%"
-
-';
+	$query = 'SELECT utilisateur.ID_Utilisateur, utilisateur.nom_Utilisateur, utilisateur.Mail_Utilisateur, utilisateur.Promo_Utilisateur, utilisateur.HuitClos_Utilisateur, 
+	invite.Entreprise_Invite, invite.Ville_Invite, invite.Nom_Invite, invite.Mail_Invite 
+	FROM utilisateur 
+	LEFT JOIN est_apprenti ON utilisateur.ID_Utilisateur = est_apprenti.ID_Utilisateur 
+	LEFT JOIN invite ON est_apprenti.ID_Invite = invite.ID_Invite 
+	JOIN habilitations ON utilisateur.ID_Utilisateur = habilitations.ID_Utilisateur 
+	JOIN etudiant_tuteur ON utilisateur.ID_Utilisateur = etudiant_tuteur.Id_etudiant
+	WHERE habilitations.Etudiant_Habilitations LIKE "oui" AND utilisateur.Annee_utilisateur LIKE "%' . $annee . '%" AND etudiant_tuteur.ID_Tuteur = '.$userId["id_Utilisateur"];
+	
 	$result = $conn->query($query);
 	$etudiants = $result->fetchAll();
 
