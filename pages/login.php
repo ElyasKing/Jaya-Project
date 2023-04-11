79fc8f491f4d50051dd5bbb16f781d0fb836f08b
Florian Borgne <flo80500@hotmail.fr>
2023-04-11 18:37:01 +0200

    11/04 : login rendu visuel plus propre

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,8 @@
 <?php
+include("../application_config/db_class.php");
+include("header.php");
 
-session_start();
+
 
 if (isset($_SESSION['flag']) && $_SESSION['flag'] == 1) {
 	$login_error = $_SESSION['flag'];
@@ -14,12 +16,22 @@ if (isset($_SESSION['flag']) && $_SESSION['flag'] == 1) {
 
 <!DOCTYPE html>
 <html>
+
+<nav class="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="index.php"><img src="../images/JAYA-LOGO2.png" alt="logo_jaya" width="180"></a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+</nav>
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="./styles.css" />
 </head>
 <body>
-
+<div class="container bg-light p-3">
 	<form method="post" action="check_login.php">
 		<h2>Connexion</h2>
 		<label for="email">Email :</label>
@@ -28,6 +40,7 @@ if (isset($_SESSION['flag']) && $_SESSION['flag'] == 1) {
 		<input type="password" id="password" name="password" required>
 		<input type="submit" value="Se connecter">
 	</form>
+</div>
 
 </body>
 </html>
