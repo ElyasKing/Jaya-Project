406a683a255bf8c8da93684932a93fa9e33befe4
Florian Borgne <flo80500@hotmail.fr>
2023-04-19 21:44:43 +0200

    19/04 : Début des maj pour les index

--- web/index_etudiant.php
+++ web/index_etudiant.php
@@ -1,48 +1,77 @@
 <?php
 $db = Database::connect();
 $annee = date('Y');
+
+$sql = getStudentInformation_Etudiant($_SESSION["user_id"]);
+
+
+$result = $db->query($sql);
+$arr_users = [];
+if ($result->rowCount() > 0) {
+    $arr_users = $result->fetchAll();
+}
 ?>
+
 
 <div class="container-fluid space">
     <h2 class="center colored">Accueil</h2>
     <hr>
     <br>
+    <h4>Information personnelles</h4>
     <br>
     <div class="panel" id="panel">
-    <table id="example" class="display" style="width:100%">
-        <thead>
-            <tr class="bg">
-                <th>col 1</th>
-                <th>col 2</th>
-                <th>col 3</th>
-                <th>col 4</th>
-                <th>col 5</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>col 1</td>
-                <td>col 2</td>
-                <td>col 3</td>
-                <td>col 4</td>
-                <td>col 5</td>
-            </tr>
-        </tbody>
-    </table>
+        <table id="example" class="display" style="width:100%">
+            <thead>
+                <tr class="bg">
+                    <th>Etudiant</th>
+                    <th>Email Etudiant</th>
+                    <th>Promo</th>
+                    <th>Entreprise</th>
+                    <th>Ville</th>
+                    <th>Maitre d'apprentissage</th>
+                    <th>Email MA</th>
+                    <th>Tuteur universitaire</th>
+                    <th>Email TU</th>
+                    <th>Huit clos</th>
+                    <th style="display:none;">Roles</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($arr_users)) { ?>
+                    <?php foreach ($arr_users as $user) { ?>
+                        <tr>
+                            <td class="text-center"><?php echo $user['Nom_Etudiant']; ?></td>
+                            <td class="text-center"><?php echo $user['Mail_Etudiant']; ?></td>
+                            <td class="text-center"><?php echo $user['Promo_Utilisateur']; ?></td>
+                            <td class="text-center"><?php echo lineFeedWithSeparator($user['Entreprise_Invite']); ?></td>
+                            <td class="text-center"><?php echo lineFeedWithSeparator($user['Ville_Invite']); ?></td>
+                            <td class="text-center"><?php echo lineFeedWithSeparator($user['Nom_Invite']); ?></td>
+                            <td class="text-center"><?php echo lineFeedWithSeparator($user['Mail_Invite']); ?></td>
+                            <td class="text-center"><?php echo $user['Nom_Tuteur_Universitaire']; ?></td>
+                            <td class="text-center"><?php echo $user['Mail_Tuteur_Universitaire']; ?></td>
+                            <td class="text-center"><?php echo $user['HuitClos_Utilisateur']; ?></td>
+                            <td class="text-center" style="display:none;"><?php echo $user['Roles']; ?>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
 
 <script>
-    $(document).ready(function () {
-        $('#example').DataTable({
+    $(document).ready(function() {
+        var table = $('#example').DataTable({
             stateSave: true,
-            language : {
-                url : "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
+            language: {
+                url: "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
             },
-            order: [[3, 'desc']],
+            order: [
+                [3, 'desc']
+            ],
             dom: 'Blfrtip',
             buttons: ['excel'],
-
         });
+
     });
 </script>
