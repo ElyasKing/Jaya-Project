64852ce421704516dd044ace1e2bacd982f2e970
Florian Borgne <flo80500@hotmail.fr>
2023-04-20 13:41:50 +0200

    20/04 : plusieurs modifications
    Index etudiant
    navbar
    Cacul des notes oral
    fonctions en +

--- web/index_administrateur.php
+++ web/index_administrateur.php
@@ -19,57 +19,63 @@ if ($result->rowCount() > 0) {
     <br>
     <br>
     <div class="panel" id="panel">
-    <table id="example" class="display" style="width:100%">
-        <thead>
-            <tr class="bg">
-                <th>Etudiant</th>
-                <th>Email Etudiant</th>
-                <th>Promo</th>
-                <th>Entreprise</th>
-                <th>Ville</th>
-                <th>Maitre d'apprentissage</th>
-                <th>Email MA</th>
-                <th>Tuteur universitaire</th>
-                <th>Email TU</th>
-                <th>Huit clos</th>
-                <th style="display:none;">Roles</th>
-                <th> </th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php if(!empty($arr_users)) { ?>
-            <?php foreach($arr_users as $user) { ?>
-                <tr <?php if(empty($user['Promo_Utilisateur']) || empty($user['Entreprise_Invite']) ||  
-                            empty($user['Ville_Invite']) || empty($user['Nom_Invite']) || empty($user['Mail_Invite']) ||  empty($user['Nom_Tuteur_Universitaire']) || 
-                            empty($user['Mail_Tuteur_Universitaire']) || empty($user['HuitClos_Utilisateur'])) { echo "style='background-color: rgba(255, 255, 0, 0.199);'";} ?> >
-                    <td class="text-center"><?= $user['Nom_Etudiant']; ?></td>
-                    <td class="text-center"><?= $user['Mail_Etudiant']; ?></td>
-                    <td class="text-center"><?= $user['Promo_Utilisateur']; ?></td>
-                    <td class="text-center"><?= lineFeedWithSeparator($user['Entreprise_Invite']); ?></td>
-                    <td class="text-center"><?= lineFeedWithSeparator($user['Ville_Invite']); ?></td>
-                    <td class="text-center"><?= lineFeedWithSeparator($user['Nom_Invite']); ?></td>
-                    <td class="text-center"><?= lineFeedWithSeparator($user['Mail_Invite']); ?></td>
-                    <td class="text-center"><?= $user['Nom_Tuteur_Universitaire']; ?></td>
-                    <td class="text-center"><?= $user['Mail_Tuteur_Universitaire']; ?></td>
-                    <td class="text-center"><?= $user['HuitClos_Utilisateur']; ?></td>
-                    <td class="text-center" style="display:none;"><?= $user['Roles']; ?>
-                    <td><a href="indexStudentUpdate_administrateur.php?id=<?= $user['ID_Etudiant'] ?>"><i class="bi bi-pencil-fill"></i></a></td>
+        <table id="example" class="display" style="width:100%">
+            <thead>
+                <tr class="bg">
+                    <th>Etudiant</th>
+                    <th>Email Etudiant</th>
+                    <th>Promo</th>
+                    <th>Entreprise</th>
+                    <th>Ville</th>
+                    <th>Maitre d'apprentissage</th>
+                    <th>Email MA</th>
+                    <th>Tuteur universitaire</th>
+                    <th>Email TU</th>
+                    <th>Huit clos</th>
+                    <th style="display:none;">Roles</th>
+                    <th> </th>
                 </tr>
-            <?php } ?>
-        <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php if (!empty($arr_users)) { ?>
+                    <?php foreach ($arr_users as $user) { ?>
+                        <tr <?php if (
+                                empty($user['Promo_Utilisateur']) || empty($user['Entreprise_Invite']) ||
+                                empty($user['Ville_Invite']) || empty($user['Nom_Invite']) || empty($user['Mail_Invite']) ||  empty($user['Nom_Tuteur_Universitaire']) ||
+                                empty($user['Mail_Tuteur_Universitaire']) || empty($user['HuitClos_Utilisateur'])
+                            ) {
+                                echo "style='background-color: rgba(255, 255, 0, 0.199);'";
+                            } ?>>
+                            <td class="text-center"><?= $user['Nom_Etudiant']; ?></td>
+                            <td class="text-center"><?= $user['Mail_Etudiant']; ?></td>
+                            <td class="text-center"><?= $user['Promo_Utilisateur']; ?></td>
+                            <td class="text-center"><?= lineFeedWithSeparator($user['Entreprise_Invite']); ?></td>
+                            <td class="text-center"><?= lineFeedWithSeparator($user['Ville_Invite']); ?></td>
+                            <td class="text-center"><?= lineFeedWithSeparator($user['Nom_Invite']); ?></td>
+                            <td class="text-center"><?= lineFeedWithSeparator($user['Mail_Invite']); ?></td>
+                            <td class="text-center"><?= $user['Nom_Tuteur_Universitaire']; ?></td>
+                            <td class="text-center"><?= $user['Mail_Tuteur_Universitaire']; ?></td>
+                            <td class="text-center"><?= $user['HuitClos_Utilisateur']; ?></td>
+                            <td class="text-center" style="display:none;"><?= $user['Roles']; ?>
+                            <td><a href="indexStudentUpdate_administrateur.php?id=<?= $user['ID_Etudiant'] ?>"><i class="bi bi-pencil-fill"></i></a></td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         var table = $('#example').DataTable({
             stateSave: true,
-            language : {
-                url : "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
+            language: {
+                url: "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
             },
-            order: [[3, 'desc']],
+            order: [
+                [0, 'asc']
+            ],
             dom: 'Blfrtip',
             buttons: ['excel'],
         });
