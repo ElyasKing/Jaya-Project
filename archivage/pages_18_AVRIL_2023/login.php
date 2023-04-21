3d038f92a4e34de93033f8f14d92eb1d9e10f20c
Jrmy1999 <75275597+Jrmy1999@users.noreply.github.com>
2023-04-21 15:01:59 +0200

    FEATURE-PASSWORD-RESET

--- archivage/pages_18_AVRIL_2023/login.php
+++ archivage/pages_18_AVRIL_2023/login.php
@@ -2,21 +2,18 @@
 include("../application_config/db_class.php");
 include("header.php");
 
-
-
 if (isset($_SESSION['flag']) && $_SESSION['flag'] == 1) {
-	$login_error = $_SESSION['flag'];
+  $login_error = $_SESSION['flag'];
 
-	if ($login_error == 1) {
-		echo "<script>  alert(\"erreur\"); </script>";
-	}
-	$_SESSION['flag'] = 0;
+  if ($login_error == 1) {
+    echo "<script>  alert(\"erreur\"); </script>";
+  }
+  $_SESSION['flag'] = 0;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <nav class="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,24 +23,24 @@ if (isset($_SESSION['flag']) && $_SESSION['flag'] == 1) {
       <a class="navbar-brand" href="index.php"><img src="../images/JAYA-LOGO2.png" alt="logo_jaya" width="180"></a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 </nav>
-<head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="./styles.css" />
-</head>
-<body>
-<div class="container bg-light p-3">
-	<form method="post" action="check_login.php">
-		<h2>Connexion</h2>
-		<label for="email">Email :</label>
-		<input type="text" id="email" name="email" required>
-		<label for="password">Mot de passe :</label>
-		<input type="password" id="password" name="password" required>
-		<!-- <form method="post" action="#"> -->
-			<!-- <label>Mot de passe oublié</label> -->
-		<!-- </form> -->
-		<input type="submit" value="Se connecter">
-	</form>
-</div>
 
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="./styles.css" />
+</head>
+
+<body>
+  <div class="container bg-light p-3">
+    <form method="post" action="check_login.php">
+      <h2>Connexion</h2>
+      <label for="email">Email :</label>
+      <input type="text" id="email" name="email" required>
+      <label for="password">Mot de passe :</label>
+      <input type="password" id="password" name="password" required>
+      <a href="forgot_password.php">Mot de passe oublié ?</a>
+      <input type="submit" value="Se connecter">
+    </form>
+  </div>
 </body>
+
 </html>
