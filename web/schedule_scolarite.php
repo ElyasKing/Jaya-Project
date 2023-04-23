c0107a83233b2768f1353959c4264c78a3cf01a2
SelaimanV <s.vanlanduyt.7@gmail.com>
2023-04-24 01:33:24 +0200

    planning reste a finir les formulaires

--- web/schedule_scolarite.php
+++ web/schedule_scolarite.php
@@ -51,6 +51,8 @@ if (!isConnectedUser()) {
                     i.Mail_Invite,
                     u1.ID_Planning,
                     p.Nom_Planning,
+                    p.DateSession_Planning,
+                    p.HeureDebutSession_Planning,
                     TRIM(BOTH ', ' FROM CONCAT_WS(', ',
                         CASE WHEN SUM(h.Admin_Habilitations = 'oui') > 0 THEN 'Admin' ELSE NULL END,
                         CASE WHEN SUM(h.ResponsableUE_Habilitations = 'oui') > 0 THEN 'ResponsableUE' ELSE NULL END,
@@ -72,10 +74,31 @@ if (!isConnectedUser()) {
                 $statement = $db->query($query);
                 $studentsList = $statement->fetchAll();
 
+                //quelle est l'année en cours ?
+                $date = getdate();
+                $currentYear = $date['year'];
+                $lastYear = $currentYear - 1;
+                $currentStudentYear = $lastYear . "-" . $currentYear;
+
+                // certains étudiants n'ont pas d'alternance et ne peuvent par conséquent pas être plannifiés.
+                $query = "SELECT DISTINCT 
+                    u1.id_Utilisateur,
+                    u1.Nom_Utilisateur AS Nom_Etudiant,
+                    u1.Annee_Utilisateur,
+                    u1.Promo_Utilisateur, 
+                    u1.HuisClos_Utilisateur 
+                FROM Utilisateur u1 
+                LEFT JOIN etudiant_tuteur et ON u1.id_Utilisateur = et.id_Etudiant
+                WHERE u1.Annee_Utilisateur = '$currentStudentYear'
+                AND u1.ID_Planning IS NULL;";
+
+                $statement = $db->query($query);
+                $studentsWithoutSchedule = $statement->fetchAll();
+                $cpt = count($studentsWithoutSchedule);
+
                 // echo "<pre>";
                 // var_dump($studentsList);
                 // echo "</pre>";die;
-
 
                 $query = "SELECT ID_Planning, Nom_Planning FROM planning";
                 $statement = $db->query($query);
@@ -100,7 +123,11 @@ if (!isConnectedUser()) {
                             }
                         }
                         echo "</select>";
+                        if($cpt > 0){
                         ?>
+                        <p class="text-center"><abbr title="Certains étudiants n'ont pas été pris en compte dans plannification. Il se peut qu'il n'aient pas été associés à un tuteur entreprise. Ils sont donc considérés comme non alternants et ne peuvent soutenir (c.f. onglet Accueil - Administrateur) : <?php foreach ($studentsWithoutSchedule as $sws) {echo $sws['Nom_Etudiant'] ." (". $sws['Promo_Utilisateur'] .")"." ; ";}?> ">
+                        <span style="color: red;" class="bi bi-exclamation-triangle-fill"></span> Etudiant non plannifiés : <?= $cpt ?></abbr></p>
+                        <?php } ?>
                     </div>
                     <table id="planning" class="display" style="width:100%">
                         <thead>
@@ -118,18 +145,50 @@ if (!isConnectedUser()) {
                         </thead>
                         <tbody>
                             <?php
+                            $query = 'SELECT Description_param FROM parametres WHERE Nom_param = "Durée d\'une session de soutenance"';
+                            $statement = $db->query($query);
+                            $time = $statement->fetch();
+
                             foreach ($studentsList as $student) {
+                                if ($student['DateSession_Planning'] != null) {
+                                    $date = DateTime::createFromFormat('Y-m-d', $student['DateSession_Planning']);
+                                    $formatted_date = $date->format('d/m/Y');
+                                }
+
                                 echo "<tr>
                                         <td class='text-center' style='display:none;'>" . $student['ID_Planning'] . "</td>
                                         <td class='text-center'>" . $student['Nom_Etudiant'] . "</td>
                                         <td class='text-center'>" . $student['Entreprise_Invite'] . "</td>
                                         <td class='text-center'>" . $student['Ville_Invite'] . "</td>
                                         <td class='text-center'>" . $student['Promo_Utilisateur'] . "</td>
-                                        <td class='text-center'>" . $student['HuisClos_Utilisateur'] . "</td>
-                                        <td></td>
-                                        <td></td>
+                                        <td class='text-center'>" . $student['HuisClos_Utilisateur'] . "</td>";
+                                if ($student['DateSession_Planning'] != null) {
+                                    $date = DateTime::createFromFormat('Y-m-d', $student['DateSession_Planning']);
+                                    $formatted_date = $date->format('d/m/Y');
+                                    echo "<td class='text-center'>" . $formatted_date . "</td>";
+                                } else {
+                                    echo "<td class='text-center'></td>";
+                                }
+                                if ($student['HeureDebutSession_Planning'] != null) {
+                                    $f1 = str_replace(":", ".", $student['HeureDebutSession_Planning']);
+                                    $sessionStartTime = (float)$f1;
+                                    $sessionStartTime = substr($sessionStartTime, 0, 5);
+                                    $num_rounded = round($sessionStartTime, 2);
+                                    $sessionStartTime2 = number_format($num_rounded, 2, 'H', '.');
+
+                                    $f2 = str_replace(":", ".", $time[0]);
+                                    $sessionEndTime = (float)$f2;
+                                    $sessionTimes =  $sessionStartTime + $sessionEndTime;
+                                    $num_rounded = round($sessionTimes, 2);
+                                    $sessionTimes = number_format($num_rounded, 2, 'H', '.');
+
+                                    echo "<td class='text-center'>" . $sessionStartTime2 . "-" . $sessionTimes . "</td>";
+                                } else {
+                                    echo "<td class='text-center'></td>";
+                                }
+                                echo "
                                         <td><a href='#?id=" . $student["ID_Etudiant"] . "&planning=" . $student["ID_Planning"] . "'><button type='button' class='btn bg bi bi-pencil-fill'></button></a></td>
-                                        </tr>";
+                                    </tr>";
                             }
                             ?>
                         </tbody>
@@ -138,7 +197,7 @@ if (!isConnectedUser()) {
             </div>
             <br>
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <form id="configure-planning-form" action="test.php" method="post">
+                <form id="configure-planning-form" action="scheduleConfiguration_scolarite.php" method="post">
                 </form>
                 <a href="#" onclick='
                     var SelectedOption = document.querySelector("#planningSelector option[selected]"); 
@@ -158,14 +217,8 @@ if (!isConnectedUser()) {
                             console.log("Erreur : " + textStatus);
                         }
                     });
-                    '><button class='btn me-md-3 btn-custom bg'>Parametrer la session</button></a>
-
-                    <?php
-echo "<pre>";
-                var_dump($_SESSION);
-                echo "</pre>";
-                    ?>
-                    
+                    '>
+                    <button id="btnConfigure" class='btn me-md-3 btn-custom bg'>Parametrer la session</button></a>
 
                 <form id="generate-planning-form" action="scheduleGeneration_scolarite.php" method="post">
                 </form>
@@ -175,101 +228,135 @@ echo "<pre>";
             </div>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-            // Récupérer la valeur sélectionnée en session
-            let selectedPlanning = sessionStorage.getItem('selectedPlanning');
-
-            $('#planning').DataTable({
-                stateSave: true,
-                language: {
-                    url: "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
-                },
-                order: [
-                    [3, 'desc']
-                ],
-                dom: 'Blfrtip',
-                buttons: ['excel'],
-
-            });
-
-            // Fonction pour filtrer les lignes en fonction de la sélection
-            function filterRows() {
-                selectedPlanning = planningSelector.value;
-                let rows = document.getElementById("planning").getElementsByTagName("tr");
-
-                for (let i = 1; i < rows.length; i++) {
-                    let row = rows[i];
-                    let planningId = row.cells[0].textContent;
-                    if (planningId === selectedPlanning || selectedPlanning === "tous") {
-                        row.style.display = "";
-                    } else {
-                        row.style.display = "none";
-                    }
-                }
-
-                // Enregistrer la valeur sélectionnée en session
-                sessionStorage.setItem('selectedPlanning', selectedPlanning);
-
-                // Réaffecter l'attribut "selected" à l'option sélectionnée
-                for (let i = 0; i < planningSelector.options.length; i++) {
-                    let option = planningSelector.options[i];
-                    if (option.value === selectedPlanning) {
-                        option.selected = true;
-                    } else {
-                        option.selected = false;
-                    }
-                }
-            }
-
-            // Sélectionner l'option enregistrée en session
-            if (selectedPlanning) {
-                planningSelector.value = selectedPlanning;
-                let oldSelectedOption = document.querySelector("#planningSelector option[selected]");
-                if (oldSelectedOption) {
-                    oldSelectedOption.removeAttribute("selected");
-                }
-                let newSelectedOption = document.querySelector("#planningSelector option:checked");
-                if (newSelectedOption) {
-                    newSelectedOption.setAttribute("selected", "");
-                }
-                filterRows();
-            }
-
-            // Filtrer les lignes lorsqu'on change la sélection
-            planningSelector.addEventListener("change", function() {
-                let oldSelectedOption = document.querySelector("#planningSelector option[selected]");
-                if (oldSelectedOption) {
-                    oldSelectedOption.removeAttribute("selected");
-                }
-                let newSelectedOption = document.querySelector("#planningSelector option:checked");
-                if (newSelectedOption) {
-                    newSelectedOption.setAttribute("selected", "");
-                }
-                filterRows();
-            });
-
-            // var SelectedOption = document.querySelector("#planningSelector option[selected]");
-            // var activeSchedule = planningSelector.value; console.log(activeSchedule);
-            // $.ajax({
-            //     url: "scheduleAjaxPostConfigurationVariable.php",
-            //     type: "POST",
-            //     data: {
-            //         activeSchedule: activeSchedule
-            //     },
-            //     "success": function(data) {
-            //         console.log("Variable enregistrée dans la session : " + data);
-            //         //document.getElementById("configure-planning-form").submit();
-            //     },
-            //     error: function(jqXHR, textStatus, errorThrown) {
-            //         console.log("Erreur : " + textStatus);
-            //     }
-            // });
-        });
-    </script>
-
-
 </body>
 
 </html>
+
+<script src="../js/toastr.min.js"></script>
+<script>
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "7000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+</script>
+<?php
+$success = $_SESSION['success'];
+switch ($success) {
+    case 1:
+        echo '<script>toastr.success("Les génération des plannings à été effectuée !");</script>';
+        break;
+    default:
+        // rien
+}
+$_SESSION['success'] = 0;
+?>
+
+<script>
+    $(document).ready(function() {
+        // Récupérer la valeur sélectionnée en session
+        let selectedPlanning = sessionStorage.getItem('selectedPlanning');
+
+        $('#planning').DataTable({
+            stateSave: true,
+            language: {
+                url: "//cdn.datatables.net/plug-ins/1.13.2/i18n/fr-FR.json"
+            },
+            order: [
+                [3, 'desc']
+            ],
+            dom: 'Blfrtip',
+            buttons: ['excel'],
+
+        });
+
+        // Fonction pour filtrer les lignes en fonction de la sélection
+        function filterRows() {
+            selectedPlanning = planningSelector.value;
+            let rows = document.getElementById("planning").getElementsByTagName("tr");
+
+            for (let i = 1; i < rows.length; i++) {
+                let row = rows[i];
+                let planningId = row.cells[0].textContent;
+                if (planningId === selectedPlanning || selectedPlanning === "tous") {
+                    row.style.display = "";
+                } else {
+                    row.style.display = "none";
+                }
+            }
+
+            // Enregistrer la valeur sélectionnée en session
+            sessionStorage.setItem('selectedPlanning', selectedPlanning);
+
+            // Réaffecter l'attribut "selected" à l'option sélectionnée
+            for (let i = 0; i < planningSelector.options.length; i++) {
+                let option = planningSelector.options[i];
+                if (option.value === selectedPlanning) {
+                    option.selected = true;
+                } else {
+                    option.selected = false;
+                }
+            }
+        }
+
+        // Sélectionner l'option enregistrée en session
+        if (selectedPlanning) {
+            planningSelector.value = selectedPlanning;
+            let oldSelectedOption = document.querySelector("#planningSelector option[selected]");
+            if (oldSelectedOption) {
+                oldSelectedOption.removeAttribute("selected");
+            }
+            let newSelectedOption = document.querySelector("#planningSelector option:checked");
+            if (newSelectedOption) {
+                newSelectedOption.setAttribute("selected", "");
+            }
+            filterRows();
+        }
+
+        // Filtrer les lignes lorsqu'on change la sélection
+        planningSelector.addEventListener("change", function() {
+            let oldSelectedOption = document.querySelector("#planningSelector option[selected]");
+            if (oldSelectedOption) {
+                oldSelectedOption.removeAttribute("selected");
+            }
+            let newSelectedOption = document.querySelector("#planningSelector option:checked");
+            if (newSelectedOption) {
+                newSelectedOption.setAttribute("selected", "");
+            }
+            filterRows();
+        });
+
+        let btnConfigure = document.querySelector('#btnConfigure');
+
+        // Vérifie s'il y a une valeur stockée en session pour le bouton Configure
+        if (sessionStorage.getItem('btnConfigureHidden') === 'true') {
+            btnConfigure.setAttribute('hidden', '');
+        } else {
+            btnConfigure.removeAttribute('hidden');
+        }
+
+        // Enregistre l'état du bouton Configure en session storage lorsque l'état change
+        planningSelector.addEventListener("change", function() {
+            let selectOption = document.querySelector("#planningSelector option[selected]");
+            if (selectOption.value === 'tous') {
+                btnConfigure.setAttribute('hidden', '');
+                sessionStorage.setItem('btnConfigureHidden', 'true');
+            } else {
+                btnConfigure.removeAttribute('hidden');
+                sessionStorage.setItem('btnConfigureHidden', 'false');
+            }
+        });
+    });
+</script>
