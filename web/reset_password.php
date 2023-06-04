69f02ee9357358737e4d07a58b7d0d37901bf32a
Jrmy1999 <75275597+Jrmy1999@users.noreply.github.com>
2023-06-04 15:31:01 +0200

    Update reset_password.php

--- web/reset_password.php
+++ web/reset_password.php
@@ -7,101 +7,88 @@ include("./home/navigation/header.php");
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Mot de passe oublié</title>
-    <link rel="stylesheet" type="text/css" href="./styles.css" />
-    <link href="../css/toastr.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container bg-light p-3">
-      <form method="post" action="reset_password.php">
-        <h2>Mot de passe oublié</h2>
-        <label for="email">Entrez votre adresse e-mail :</label>
-        <input type="email" id="email" name="email" required>
-        <input type="submit" name="submit" value="Réinitialiser le mot de passe">
-      </form>
+
+<head>
+  <title>Mot de passe oublié</title>
+  <link rel="stylesheet" type="text/css" href="./styles.css" />
+  <?php
+  include("./home/navigation/navbar.php");
+  ?>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-12">
+        <div class="card shadow-2-strong css-login" style="width: auto; max-width: 1200px;">
+          <div class="card-body p-5 text-left" style="min-width: 1100px; max-width: 100%; overflow-x: auto;">
+            <form method="post" action="reset_password.php">
+              <label for="email">Entrez votre adresse e-mail :</label>
+              <input type="email" id="email" name="email" required>
+              <input type="submit" name="submit" value="Réinitialiser le mot de passe">
+            </form>
+            <?php if (isset($_SESSION['reset_password_success'])): ?>
+              <div class="alert alert-success">
+                <?php echo $_SESSION['reset_password_success']; unset($_SESSION['reset_password_success']); ?>
+              </div>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['reset_password_error'])): ?>
+              <div class="alert alert-danger">
+                <?php echo $_SESSION['reset_password_error']; unset($_SESSION['reset_password_error']); ?>
+              </div>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</body>
 
-    <script src="../js/toastr.min.js"></script>
-    <script>
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": true,
-            "positionClass": "toast-top-center",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "7000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-    </script>
-
-    <?php if (isset($_SESSION['reset_password_success'])): ?>
-      <script>toastr.success("<?php echo $_SESSION['reset_password_success']; unset($_SESSION['reset_password_success']); ?>");</script>
-    <?php endif; ?>
-
-    <?php if (isset($_SESSION['reset_password_error'])): ?>
-      <script>toastr.error("<?php echo $_SESSION['reset_password_error']; unset($_SESSION['reset_password_error']); ?>");</script>
-    <?php endif; ?>
-  </body>
 </html>
 
 <?php
-require_once("../application_config/db_class.php");
-
-ini_set('SMTP', '127.0.0.1');
-ini_set('smtp_port', 25);
-
-if(isset($_POST['submit'])){
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
 
-// vérifie que l'adresse email existe dans la table utilisateur
-$query = "SELECT ID_Utilisateur, Nom_Utilisateur, Mail_Utilisateur FROM utilisateur WHERE Mail_Utilisateur = :email";
-$stmt = Database::connect()->prepare($query);
-$stmt->bindParam(":email", $email);
-$stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // vérifie que l'adresse email existe dans la table utilisateur
+    $query = "SELECT ID_Utilisateur, Nom_Utilisateur, Mail_Utilisateur FROM utilisateur WHERE Mail_Utilisateur = :email";
+    $stmt = Database::connect()->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-var_dump($user); // Ajout de cette ligne pour vérifier les données utilisateur récupérées
+    if ($user === false) {
+        $_SESSION['reset_password_error'] = "Adresse email non trouvée.";
+        header('Location: reset_password.php');
+        exit();
+    }
 
-if($user === false){
-    $_SESSION['reset_password_error'] = "Adresse email non trouvée.";
-    header('Location: reset_password.php');
-    exit();
+    // génère un token unique et l'insère dans la table password_reset_tokens
+    $token = bin2hex(random_bytes(16));
+    $query = "INSERT INTO password_reset_tokens (user_id, token, Mail_Utilisateur) VALUES (:user_id, :token, :Mail_Utilisateur)";
+    $stmt = Database::connect()->prepare($query);
+    $stmt->bindParam(":user_id", $user['ID_Utilisateur']);
+    $stmt->bindParam(":token", $token);
+    $stmt->bindParam(":Mail_Utilisateur", $user['Mail_Utilisateur']);
+    $stmt->execute();
+
+    // Envoyer un email
+    $subject = "Réinitialisation de votre mot de passe";
+    $reset_link = "http://localhost/Jaya-Project/web/reset_password_action.php?token=" . $token;
+    $body = "<p>Vous avez demandé à réinitialiser votre mot de passe. Cliquez sur le lien suivant pour le réinitialiser :</p>
+            <p><a href='$reset_link'>$reset_link</a></p>
+            <p>Si vous n'avez pas demandé de réinitialisation de mot de passe, veuillez ignorer cet email.</p>";
+    $send_email_status = send_email($email, $subject, $body);
+
+    if (strpos($send_email_status, "Erreur lors de l'envoi de l'email") !== false) {
+        $_SESSION['reset_password_error'] = "Une erreur est survenue lors de l'envoi de l'e-mail de réinitialisation de mot de passe.";
+        header('Location: reset_password.php');
+        exit();
+    } else {
+        $_SESSION['reset_password_success'] = "Un e-mail de réinitialisation de mot de passe a été envoyé à votre adresse e-mail.";
+        header('Location: reset_password.php');
+        exit();
+    }
 }
-
-// génère un token unique et l'insère dans la table password_reset_tokens
-$token = bin2hex(random_bytes(16));
-$query = "INSERT INTO password_reset_tokens (user_id, token, Mail_Utilisateur) VALUES (:user_id, :token, :Mail_Utilisateur)";
-$stmt = Database::connect()->prepare($query);
-$stmt->bindParam(":user_id", $user['ID_Utilisateur']);
-$stmt->bindParam(":token", $token);
-$stmt->bindParam(":Mail_Utilisateur", $user['Mail_Utilisateur']);
-$stmt->execute();
-
- // envoie un e-mail à l'utilisateur avec le lien de réinitialisation
- $to = $email;
- $subject = "Réinitialisation de votre mot de passe JAYA";
- $message = "Bonjour " . $user['Nom_Utilisateur'] . ",\n\n";
- $message .= "Veuillez cliquer sur le lien suivant pour réinitialiser votre mot de passe JAYA : \n";
- $message .= "http://localhost/Jaya-Project/pages/new_password.php?token=" . $token;
- $headers = "From: jaya-project@example.com";
- 
- if(mail($to, $subject, $message, $headers)){
-     $_SESSION['reset_password_success'] = "Un e-mail de réinitialisation de mot de passe a été envoyé à votre adresse e-mail.";
- } else {
-     $_SESSION['reset_password_error'] = "Une erreur est survenue lors de l'envoi de l'e-mail de réinitialisation de mot de passe.";
- }
- 
- header('Location: reset_password.php');
- exit();
- }
- ?>
+?>
